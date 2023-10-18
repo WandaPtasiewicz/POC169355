80748473e025b7d4bd3d377661888d9706ca0516
WandaPtasiewicz <115210825+WandaPtasiewicz@users.noreply.github.com>
2023-10-18 10:10:48 +0200

    zadania 18.10.23

---
+++ lekcja 18.10.23/src/zadAKT.java
@@ -0,0 +1,11 @@
+public class zadAKT {
+    public static void main(String[] args) {
+        double b= 56.5;
+        System.out.println(b);
+        System.out.println(stopnieNaRadioany(b));
+    }
+    public static double stopnieNaRadioany(double a){
+        a=a*3.14 /180;
+        return a;
+    }
+}
