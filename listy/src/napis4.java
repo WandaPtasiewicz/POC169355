0c19662e118a6a9ebe046d3d505a953fc48e8be2
WandaPtasiewicz <wanda.ptasiewicz@gmail.com>
2023-10-22 21:21:06 +0200

    zadanie napis5

---
+++ listy/src/napis4.java
@@ -0,0 +1,12 @@
+import java.lang.StringBuilder;
+public class napis4 {
+    public static void main(String[] args) {
+        String napis = "hmm";
+        StringBuilder mam= new StringBuilder("lalka");
+        mam.replace(0,1,"L");
+        mam.replace(4,5,"A");
+        System.out.println(mam);
+
+    }
+
+}
