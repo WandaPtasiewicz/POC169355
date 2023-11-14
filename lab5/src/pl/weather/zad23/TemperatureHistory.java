a942a3835bf9d45cb3f5d6bfcda76a550418e9ba
WandaPtasiewicz <wanda.ptasiewicz@gmail.com>
2023-11-14 15:54:09 +0100

    zad 23 i 5 poprawione

--- lab5/src/pl/weather/zad23/TemperatureHistory.java
+++ lab5/src/pl/weather/zad23/TemperatureHistory.java
@@ -3,13 +3,14 @@ package pl.weather.zad23;
 import java.util.ArrayList;
 
 class TemperatureHistory {
-    private ArrayList <Double>temperatures;
+    private ArrayList <Double>temperatures = new ArrayList<>();
 
     public TemperatureHistory(){
 
     }
 
     public ArrayList<Double> getTemperatures() {
+
         return temperatures;
     }
 
@@ -17,16 +18,16 @@ class TemperatureHistory {
         this.temperatures = temperatures;
     }
 
-    public void addTemperature(Double temperature){
+    public void addTemperature( Double temperature){
         this.temperatures.add(temperature);
     }
     public void removeTemperature(int index){
         this.temperatures.remove(index);
     }
-    public Double averageResult(){
-        double wynik=0;
-        for(int i=0;i<this.temperatures.size()-1;i++){
-            wynik+=this.temperatures.get(i);
+    public double averageResult(){
+         Double wynik=0.0;
+        for(int i=0;i<this.temperatures.size();i++){
+             wynik+=this.temperatures.get(i);
         }
         return wynik/this.temperatures.size();
     }
--- lab5/src/pl/weather/zad23/TestTemperatureHistory.java
+++ lab5/src/pl/weather/zad23/TestTemperatureHistory.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 class TestTemperatureHistory {
     public static void main(String[] args) {
         TemperatureHistory t1= new TemperatureHistory();
-        t1.addTemperature(3.5);
-        t1.addTemperature(342.5);
-        t1.addTemperature(1.5);
-        t1.addTemperature(36.5);
-        t1.addTemperature(30.5);
+        t1.addTemperature(1.0);
+        t1.addTemperature(2.0);
+        t1.addTemperature(2.0);
+        t1.addTemperature(1.0);
+        t1.addTemperature(2.4);
+        System.out.println(t1.getTemperatures());
         t1.removeTemperature(2);
         System.out.println(t1.averageResult());
 
